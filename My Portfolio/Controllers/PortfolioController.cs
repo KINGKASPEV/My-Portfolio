e657e900f4a21a9c358d0480ab0e432f0e6fc08c
Kingkaspev <kingsleychiboy22@gmail.com>
2023-11-13 16:03:24 +0100

    My Portfolio Website

---
+++ My Portfolio/Controllers/PortfolioController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using My_Portfolio.Models;
+
+namespace My_Portfolio.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        public IActionResult Index()
+        {
+            var portfolioItems = GetPortfolioItems();
+
+            return View(portfolioItems);
+        }
+
+        private List<PortfolioItem> GetPortfolioItems()
+        {
+            // This is just an example, replace it with actual data retrieval logic
+            return new List<PortfolioItem>
+            {
+                new PortfolioItem
+                {
+                    Title = "Project 1",
+                    Description = "Description of Project 1",
+                    ProjectUrl = "https://project1.com",
+                    GitHubRepository = "https://github.com/project1",
+                    TechnologiesUsed = new List<string> { "ASP.NET Core", "React", "MongoDB" },
+                    DateCompleted = new DateTime(2022, 5, 15)
+                },
+            };
+        }
+    }
+}
